86e757e73f35f35eab51dfac22bf667feb683364
Tristan Myers <tristifano@gmail.com>
2019-08-07 06:49:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(food)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    food.include?(cheese) ? cheese : 
 end
